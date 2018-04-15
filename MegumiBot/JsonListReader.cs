e7379841e4c9ea7cd9a88eaaec6c317671bc1c19
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-04-14 22:02:19 -0400

    Add generic JSON list reader.

---
+++ MegumiBot/JsonListReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MegumiBot
+{
+	public class JsonListReader<T>
+	{
+		public JsonListReader(string filePath)
+		{
+			FilePath = filePath;
+		}
+
+		private List<T> _itemList = new List<T>();
+
+		public string FilePath { get; }
+
+		public string ReadAllText()
+		{
+			return GetOrCreateFile(FilePath);
+		}
+
+		public void Add(T item)
+		{
+			_itemList.Add(item);
+		}
+
+		public List<T> ItemList => _itemList;
+
+		private string GetOrCreateFile(string filePath)
+		{
+			if (!File.Exists(FilePath))
+			{
+				File.WriteAllText(filePath, "{}");
+				return "{}";
+			}
+
+			return File.ReadAllText(filePath);
+		}
+	}
+}
