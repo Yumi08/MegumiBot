bd97835ab83b2c0818f158262c98a192aecfb1fa
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-04-15 21:58:32 -0400

    Add ability to search by artist, copyright, and character as well to ?Yubooru command.

--- MegumiBot/Modules/Nsfw.cs
+++ MegumiBot/Modules/Nsfw.cs
@@ -12,6 +12,11 @@ namespace MegumiBot.Modules
 {
 	public class Nsfw : ModuleBase<SocketCommandContext>
 	{
+		/// <summary>
+		/// Get a lewd image on Yumi08's personal hentai collection ;>
+		/// </summary>
+		/// <param name="tag"></param>
+		/// <returns></returns>
 		[Command("yubooru")]
 		public async Task Yubooru(string tag)
 		{
@@ -26,7 +31,45 @@ namespace MegumiBot.Modules
 			var imageInfoPath = Config.Bot.YubooruLocation + "\\ImageInfo.json";
 			var json = File.ReadAllText(imageInfoPath);
 			var images = JsonConvert.DeserializeObject<List<Image>>(json);
-			var matchingImages = images.Where(i => i.Tags.Any(t => t.Equals(tag, StringComparison.CurrentCultureIgnoreCase))).ToList();
+
+			List<Image> matchingImages = new List<Image>();
+
+			#region Match Detection
+
+			matchingImages.AddRange(images.Where(i =>
+			{
+				if (i.Tags == null) return false;
+
+				return i.Tags
+					.Any(t => t.Equals(tag, StringComparison.CurrentCultureIgnoreCase));
+			}));
+
+			matchingImages.AddRange(images.Where(i =>
+			{
+				if (i.Characters == null) return false;
+
+				return i.Characters
+					.Any(t => t.Equals(tag, StringComparison.CurrentCultureIgnoreCase));
+			}));
+
+			matchingImages.AddRange(images.Where(i =>
+			{
+				if (i.Copyrights == null) return false;
+
+				return i.Copyrights
+					.Any(t => t.Equals(tag, StringComparison.CurrentCultureIgnoreCase));
+			}));
+
+			matchingImages.AddRange(images.Where(i =>
+			{
+				if (i.Artists == null) return false;
+
+				return i.Artists
+					.Any(t => t.Equals(tag, StringComparison.CurrentCultureIgnoreCase));
+			}));
+
+			#endregion
+
 			var image = matchingImages[Global.Random.Next(matchingImages.Count)];
 			var imageFile = directory.GetFiles().FirstOrDefault(f => Path.GetFileNameWithoutExtension(f.Name) == image.Id);
 
