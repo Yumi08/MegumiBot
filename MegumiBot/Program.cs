1a9ce6572d41c36ab694a24938fc84b5f397f499
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-04-17 17:33:14 -0400

    Add console command.

--- MegumiBot/Program.cs
+++ MegumiBot/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Discord.WebSocket;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using MegumiBot.Core;
@@ -11,6 +14,7 @@ namespace MegumiBot
     {
         private DiscordSocketClient _client;
         private CommandHandler _handler;
+	    private bool _clientIsReady;
 
         static void Main()
         => new Program().StartAsync().GetAwaiter().GetResult();
@@ -24,13 +28,105 @@ namespace MegumiBot
             });
             _client.Log += Log;
 	        _client.Ready += AutoSave.StartTimer;
+	        _client.Ready += () =>
+	        {
+		        _clientIsReady = true;
+		        return Task.CompletedTask;
+	        };
             await _client.LoginAsync(TokenType.Bot, Config.Bot.Token);
             await _client.StartAsync();
             Global.Client = _client;
             _handler = new CommandHandler();
-            await _handler.InitializeAsync(_client);
+			await _handler.InitializeAsync(_client);
+			// Don't await
+	        ConsoleInput();
             await Task.Delay(-1);
         }
+
+	    private async Task ConsoleInput()
+	    {
+		    var input = string.Empty;
+			while (input?.Trim().ToLower() != "block")
+			{
+				input = Console.ReadLine();
+
+				switch (input?.Trim().ToLower())
+				{
+					case "message":
+						ConsoleSendMessage();
+						break;
+				}
+			}
+	    }
+
+	    private async void ConsoleSendMessage()
+	    {
+			Console.WriteLine("Select the guild:");
+		    var guild = GetSelected(_client.Guilds);
+		    var textChannel = GetSelected(guild.TextChannels);
+		    var msg = string.Empty;
+		    while (msg.Trim() == string.Empty)
+		    {
+			    Console.WriteLine("Your message:");
+			    msg = Console.ReadLine();
+		    }
+
+		    await textChannel.SendMessageAsync(msg);
+	    }
+
+	    private static SocketGuild GetSelected(IEnumerable<SocketGuild> guilds)
+	    {
+		    var guildsList = guilds.ToList();
+			var maxIndex = guildsList.Count - 1;
+
+		    for (var i = 0; i <= maxIndex; i++)
+		    {
+			    Console.WriteLine($"{i} - {guildsList[i].Name}");
+		    }
+
+		    var selectedIndex = -1;
+		    while (selectedIndex < 0 || selectedIndex > maxIndex)
+		    {
+			    var success = int.TryParse(Console.ReadLine()?.Trim(), out selectedIndex);
+			    if (!success)
+			    {
+					Console.WriteLine("Please provide a valid index.");
+				    selectedIndex = -1;
+			    }
+
+				if (selectedIndex < 0 || selectedIndex > maxIndex)
+					Console.WriteLine("The index didn't fit within the guild list, please try again.");
+		    }
+
+		    return guildsList[selectedIndex];
+	    }
+
+	    private static SocketTextChannel GetSelected(IEnumerable<SocketTextChannel> channels)
+	    {
+		    var socketTextChannels = channels.ToList();
+		    var maxIndex = socketTextChannels.Count - 1;
+
+		    for (var i = 0; i <= maxIndex; i++)
+		    {
+			    Console.WriteLine($"{i} - {socketTextChannels[i].Name}");
+		    }
+
+		    var selectedIndex = -1;
+		    while (selectedIndex < 0 || selectedIndex > maxIndex)
+		    {
+			    var success = int.TryParse(Console.ReadLine()?.Trim(), out selectedIndex);
+			    if (!success)
+			    {
+				    Console.WriteLine("Please provide a valid index.");
+				    selectedIndex = -1;
+			    }
+
+			    if (selectedIndex < 0 || selectedIndex > maxIndex)
+				    Console.WriteLine("The index didn't fit within the channel list, please try again.");
+		    }
+
+		    return socketTextChannels[selectedIndex];
+	    }
 
         private async Task Log(LogMessage msg)
         {
